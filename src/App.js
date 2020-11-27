e107f787f9b09357c6af81ef2ea11c002818efd7
javante_nelson <javantenelson@gmail.com>
2020-11-26 20:09:30 -0600

    fixed error with npm start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './index.css'
 import React, { Component } from 'react';
-
+fiximport Navbar from './components/Navbar'
 
 
 class App extends Component {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,3 +2,17 @@ import React, { useState } from 'react'
 import {Button} from './Button'
 import "./Navbar.css"
 import { Link } from 'react-router-dom'
+
+function Navbar(){
+    return (
+        <>
+        <nav className="navbar">
+            <Link to="/" className='navbar-logo'>
+                Epic
+            </Link>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar;
